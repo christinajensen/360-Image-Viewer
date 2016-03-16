b0d9c11c98a3ff857ffea4dc82d2cf3b73200124
Christina Jensen <cj.christina.jensen@gmail.com>
2016-03-15 17:54:55 -0700

    placing camera inside sphere and adding orbit controls

--- app.js
+++ app.js
@@ -2,28 +2,42 @@ window.onload = function() {
 
 // add scene
 var scene = new THREE.Scene();
-// camera and set camera position
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+
+// add camera and set camera position
+var camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
 camera.position.x = 0.1;
 
 // add renderer and append it to the DOM
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+  
+// renderer.setFaceCulling( "front", "ccw" );
 
-// adding light
-var light = new THREE.DirectionalLight(0xffffff);
-    light.position.set(0, 1, 1).normalize();
-    scene.add(light);
+// add light
+var light = new THREE.AmbientLight(0xffffff);
+// light.position.set(0, 1, 1).normalize();
+scene.add(light);
 
 // create sphere
-var geometry = new THREE.SphereGeometry(20, 32, 32);
+var geometry = new THREE.SphereGeometry(100, 32, 32);
 var material = new THREE.MeshPhongMaterial({ map: THREE.ImageUtils.loadTexture('360-nature.jpg') });
 var sphere = new THREE.Mesh(geometry, material);
-// add sphere to the scene
-sphere.position.z = -50;
-// sphere.scale.x = -1;
+// material.side = THREE.DoubleSide;
+
+// inverte sphere
+sphere.scale.x = -1;
+
+// set position of and add sphere to the scene
+// sphere.position.z = -50;
 scene.add(sphere);
+
+// rotate camera with the mouse
+var controls = new THREE.OrbitControls(camera);
+controls.noPan = true;
+controls.noZoom = true; 
+controls.autoRotate = true;
+controls.autoRotateSpeed = 0.5;
 
 // render
 var render = function(){
