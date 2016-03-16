255d2015c8398b666fd649b79ffce0fbc378e8e3
Christina Jensen <cj.christina.jensen@gmail.com>
2016-03-16 10:43:43 -0700

    adding head tracking for mobile

--- app.js
+++ app.js
@@ -4,48 +4,46 @@ window.onload = function() {
 var scene = new THREE.Scene();
 
 // add camera and set camera position
-var camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 5000);
 camera.position.x = 0.1;
 
 // add renderer and append it to the DOM
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-  
-// renderer.setFaceCulling( "front", "ccw" );
+
+// apply VR stereo rendering to renderer
+var effect = new THREE.VREffect(renderer);
+effect.setSize(window.innerWidth, window.innerHeight);
+
+// create camera controller for head tracking when on a mobile device
+var controls = new THREE.VRControls(camera);
+
+// create a VR manager helper to enter and exit VR mode
+var manager = new WebVRManager(renderer, effect);
 
 // add light
 var light = new THREE.AmbientLight(0xffffff);
-// light.position.set(0, 1, 1).normalize();
 scene.add(light);
 
 // create sphere
 var geometry = new THREE.SphereGeometry(100, 32, 32);
 var material = new THREE.MeshPhongMaterial({ map: THREE.ImageUtils.loadTexture('360-nature.jpg') });
 var sphere = new THREE.Mesh(geometry, material);
-// material.side = THREE.DoubleSide;
 
-// inverte sphere
+// inverte sphere to 'look inside'
 sphere.scale.x = -1;
 
-// set position of and add sphere to the scene
-// sphere.position.z = -50;
+// add sphere to the scene
 scene.add(sphere);
-
-// rotate camera with the mouse
-var controls = new THREE.OrbitControls(camera);
-controls.noPan = true;
-controls.noZoom = true; 
-controls.autoRotate = true;
-controls.autoRotateSpeed = 0.5;
 
 // render
 var render = function(){
-      requestAnimationFrame(render);
-      // sphere.rotation.x += .01;
-      // sphere.rotation.y += .01;
-      renderer.render(scene, camera);
-    };
+  controls.update();
+  requestAnimationFrame(render);
+  manager.render(scene, camera);
+};
 
 render();
 
